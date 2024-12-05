e971b5f218c7fd108d0e3b254bd2955a67966f99
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-05 11:12:43 -0500

    Initial commit

---
+++ src/main/java/com/playground/GraphQL_Federated_Clients/GraphQlFederatedClientsApplication.java
@@ -0,0 +1,13 @@
+package com.playground.GraphQL_Federated_Clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlFederatedClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlFederatedClientsApplication.class, args);
+	}
+
+}
